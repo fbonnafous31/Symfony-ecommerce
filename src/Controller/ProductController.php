2be4201f0ef24d9f6c161e31239d837f5b944f70
François Bonnafous <francois.bonnafous@hotmail.fr>
2022-11-11 18:56:15 +0100

    Découverte de fonctionnalités avancées du composant Form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use ErrorException;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
@@ -12,8 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
