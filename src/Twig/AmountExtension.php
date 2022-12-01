2550786a59ce59cd2e03c5c9eae92bc67940adf2
François Bonnafous <francois.bonnafous@hotmail.fr>
2022-12-01 16:55:04 +0100

    Ajout du filtre Twig amount

---
+++ src/Twig/AmountExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\TwigFilter;
+use Twig\Extension\AbstractExtension;
+
+class AmountExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('amount', [$this, 'amount'])
+        ];
+    }
+
+    public function amount($value, string $symbol = '€', string $decsep = ',', string $thousandsep = ' ')
+    {
+        $finalValue = $value / 100;
+        $finalValue = number_format($finalValue, 2, $decsep, $thousandsep);
+
+        return $finalValue . ' ' . $symbol;
+    }
+}
